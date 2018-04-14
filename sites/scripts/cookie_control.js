f4f16cc5fc02eb1963881f1572e9f3cab60c65bc
Jakub Stanislawczyk <jakubstanislawczyk98@gmail.com>
2018-04-14 20:26:06 +0200

    changes in login page

--- sites/scripts/cookie_control.js
+++ sites/scripts/cookie_control.js
@@ -18,3 +18,14 @@ function deleteAllCookies() {
 		document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT";
 	}
 }
+
+function readCookie(name) {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0;i < ca.length;i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') c = c.substring(1,c.length);
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+    }
+    return null;
+}
