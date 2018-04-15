a700e0857b1cca6a6a2418c3c4dd147ba7ee2657
Jakub Stanislawczyk <jakubstanislawczyk98@gmail.com>
2018-04-15 20:21:53 +0200

    user can register his presence at course

--- sites/scripts/setup_student.js
+++ sites/scripts/setup_student.js
@@ -1,5 +1,5 @@
-function setupStudent(email, password){
-				
+function setupStudent(email, password){	
+	
 	$.ajax({
 		url: 'http://localhost:8080/course/show_all_courses',
 		method: 'GET',
@@ -17,8 +17,60 @@ function setupStudent(email, password){
 				$('.main').append(
 					'<div class="course"><div class="courseName"><b>'+course.name
 					+'</b><br />'+course.date+'</div>'
-					+'<div class="courseDescription">'+course.description+'</div><div style="clear: both"></div></div>');
+					+'<div class="courseDescription">'+course.description+'<input id="'+course.id+'" onclick="registerPresence(this.id)" class="registerPresenceButton" type="button" value="V"/>'+'</div><div style="clear: both"></div></div>');
 			});
+			changeButtonsStatus(readCookie("id"), readCookie("email"), readCookie("password"));
 		}
 	});	
 }
+
+function registerPresence(courseId){
+	let userId = readCookie("id");
+	let password = readCookie("password");
+	let email = readCookie("email");
+	let url = 'http://localhost:8080/student/registerPresence?courseId='+courseId+'&userId='+userId;
+	
+	$.ajax({
+		url: url,
+		method: 'POST',
+		crossDomain: true,
+		beforeSend: function ( xhr ) {
+			xhr.setRequestHeader( 'Authorization', 'Basic ' + btoa( email+':'+password) );
+		},
+		error: function(){
+			console.log("error");
+		},
+		success: function(txtStatus, xhr ) {
+			document.getElementById(courseId).classList.add('registeredPresensceLabel');
+			document.getElementById(courseId).classList.remove('registerPresenceButton');
+			document.getElementById(courseId).disabled = true;
+		}
+	});
+}
+
+function changeButtonsStatus(userId, email, password){
+	
+	$.ajax({
+		url: 'http://localhost:8080/student/checkCoursesStatus/'+userId,
+		method: 'GET',
+		contentType: 'application/json',				
+		dataType: 'json',
+		crossDomain: true,
+		beforeSend: function ( xhr ) {
+			xhr.setRequestHeader( 'Authorization', 'Basic ' + btoa( email+':'+password) );
+		},
+		error: function(){
+			console.log("error");
+		},
+		success: function(data, txtStatus, xhr) {
+			$.each(data, function(index, courseId) {
+				document.getElementById(courseId).classList.add('registeredPresensceLabel');
+				document.getElementById(courseId).classList.remove('registerPresenceButton');
+				document.getElementById(courseId).disabled = true;
+			});
+		}
+	});
+}
+
+
+
