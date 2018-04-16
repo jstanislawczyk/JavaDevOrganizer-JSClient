fec8803caf736ab3210fa1406bb689a0124986b8
Jakub Stanislawczyk <jakubstanislawczyk98@gmail.com>
2018-04-16 20:17:31 +0200

    User can't click presence button before course date

--- sites/scripts/setup_student.js
+++ sites/scripts/setup_student.js
@@ -13,23 +13,40 @@ function setupStudent(email, password){
 			console.log("error");
 		},
 		success: function( data, txtStatus, xhr ) {
+			
+			let date = new Date();
+			let todayDateInMillis = date.getTime();
+							
 			$.each(data, function(index, course) {
-				$('.main').append(
+				
+				let courseDate = new Date(course.date);
+				let courseDateInMills = courseDate.getTime();
+				
+				if(courseDateInMills<=todayDateInMillis){
+					$('.main').append(
 					'<div class="course"><div class="courseName"><b>'+course.name
 					+'</b><br />'+course.date+'</div>'
-					+'<div class="courseDescription">'+course.description+'<input id="'+course.id+'" onclick="registerPresence(this.id,'+course.date+')" class="registerPresenceButton" type="button" value="V"/>'+'</div><div style="clear: both"></div></div>');
+					+'<div class="courseDescription">'+course.description+'<input id="'+course.id+'" onclick="registerPresence(this.id)" class="registerPresenceButton" type="button" value="V"/>'
+					+'</div><div style="clear: both"></div></div>');
+				}else{
+					$('.main').append(
+					'<div class="course"><div class="courseName"><b>'+course.name
+					+'</b><br />'+course.date+'</div>'
+					+'<div class="courseDescription">'+course.description+'<input id="'+course.id+'" onclick="registerPresence(this.id)" class="disabledPresenceButton" type="button" value="V"  disabled/>'
+					+'</div><div style="clear: both"></div></div>');	
+				}		
 			});
 			changeButtonsStatus(readCookie("id"), readCookie("email"), readCookie("password"));
 		}
 	});	
 }
 
-function registerPresence(courseId, courseDate){
+function registerPresence(courseId){
 	let userId = readCookie("id");
 	let password = readCookie("password");
 	let email = readCookie("email");
 	let url = 'http://localhost:8080/student/registerPresence?courseId='+courseId+'&userId='+userId;
-	
+
 	$.ajax({
 		url: url,
 		method: 'POST',
