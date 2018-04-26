9eca6f8584fb4bcfc1994ef0c0845d15c0e81bf1
Jakub Stanislawczyk <jakubstanislawczyk98@gmail.com>
2018-04-26 23:44:18 +0200

    New course table in lecturer and admin page

---
+++ sites/scripts/setup_admin_courses.js
@@ -0,0 +1,65 @@
+$(document).ready(function() {
+	
+	let password = readCookie("password");
+	let email = readCookie("email");
+	
+    $.ajax({
+		url: 'http://localhost:8080/courses',
+		method: 'GET',
+		contentType: 'application/json',				
+		dataType: 'json',
+		crossDomain: true,
+		beforeSend: function ( xhr ) {
+			xhr.setRequestHeader( 'Authorization', 'Basic ' + btoa( email+':'+password) );
+		},
+		error: function(){
+			console.log("error");
+		},
+		success: function( data, txtStatus, xhr ) {
+			
+			let date = new Date();
+			let todayDateInMillis = date.getTime();
+							
+			$.each(data, function(index, course) {
+				
+				let courseDate = new Date(course.date);
+				let courseDateInMills = courseDate.getTime();
+				
+				if(courseDateInMills>=todayDateInMillis){
+					$('.main').append(
+					'<div class="course"><div class="courseName"><b>'+course.name
+					+'</b><br />'+course.date+'</div>'
+					+'<div class="courseDescription">'+course.description+'<input id="'+course.id+'" onclick="deleteCourse(this.id)" class="registerPresenceButton" type="button" value="X"/>'
+					+'</div><div style="clear: both"></div></div>');
+				}else{
+					$('.main').append(
+					'<div class="course"><div class="courseName"><b>'+course.name
+					+'</b><br />'+course.date+'</div>'
+					+'<div class="courseDescription">'+course.description+'<input id="'+course.id+'" onclick="deleteCourse(this.id)" class="disabledPresenceButton" type="button" value="X"  disabled/>'
+					+'</div><div style="clear: both"></div></div>');	
+				}		
+			});
+		}
+	});
+});
+
+function deleteCourse(courseId){
+	let password = readCookie("password");
+	let email = readCookie("email");
+	let url = 'http://localhost:8080/course/'+courseId;
+
+	$.ajax({
+		url: url,
+		type: 'DELETE',
+		crossDomain: true,
+		beforeSend: function ( xhr ) {
+			xhr.setRequestHeader( 'Authorization', 'Basic ' + btoa( email+':'+password) );
+		},
+		error: function(){
+			console.log("error");
+		},
+		success: function(txtStatus, xhr ) {
+			location.reload();
+		}
+	});
+}
--- sites/scripts/setup_lecturer.js
+++ sites/scripts/setup_lecturer.js
@@ -1,4 +1,3 @@
 function setupLecturer(email, password){
-	$('.menu').append('<input id="controlStudents" class="menuButton type="button" value="Studenci" />');
-	$('.menu').append('<input id="controlCourses" class="menuButton type="button" value="Zajęcia" />');
+	
 }
